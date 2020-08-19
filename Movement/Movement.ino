63b93068141c6f2f384145e7067625af6efd7077
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2020-08-19 12:20:15 +0530

    Update Movement.ino

--- Movement/Movement.ino
+++ Movement/Movement.ino
@@ -1,16 +1,14 @@
 
 #define Motor_Left_1_F 1                      
-#define Motor_Left_1_B 2           //Motor Driver 1
+#define Motor_Left_1_B 2           
 #define Motor_Right_1_F 3 
 #define Motor_Right_1_B 4
-
 #define Motor_Left_2_F 5
-#define Motor_Left_2_B 6          //Motor Driver 2
+#define Motor_Left_2_B 6         
 #define Motor_Right_2_F 7 
 #define Motor_Right_2_B 8
-
 #define Motor_Left_3_F 9 
-#define Motor_Left_3_B 10         //Motor Driver 3
+#define Motor_Left_3_B 10         
 #define Motor_Right_3_F 11
 #define Motor_Right_3_B 12 
 
